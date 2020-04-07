b2c59715a80871d931a9c4fec40d7d10c3e3616a
hai-27 <2601615158@qq.com>
2020-04-07 23:26:06 +0800

    Update koa body middleware.

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
  * @Author: hai-27
  * @Date: 2020-02-07 16:51:56
  * @LastEditors: hai-27
- * @LastEditTime: 2020-04-07 23:12:53
+ * @LastEditTime: 2020-04-07 23:23:00
  */
 const Koa = require('koa');
 const KoaStatic = require('koa-static');
 const KoaBody = require('koa-body');
 const Session = require('koa-session');
 
-let { Port, staticDir, uploadDir } = require('./config');
+let { Port, staticDir } = require('./config');
 
 let app = new Koa();
 
@@ -42,20 +42,8 @@ app.use(async (ctx, next) => {
 });
 
 // 处理请求体数据
-app.use(KoaBody({
-  multipart: true,
-  // parsedMethods默认是['POST', 'PUT', 'PATCH']
-  parsedMethods: ['POST', 'PUT', 'PATCH', 'GET', 'HEAD', 'DELETE'],
-  formidable: {
-    uploadDir: uploadDir, // 设置文件上传目录
-    keepExtensions: true, // 保持文件的后缀
-    maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小限制
-    onFileBegin: (name, file) => { // 文件上传前的设置
-      // console.log(`name: ${name}`);
-      // console.log(file);
-    }
-  }
-}));
+const koaBodyConfig = require('./app/middleware/koaBodyConfig');
+app.use(KoaBody(koaBodyConfig));
 
 // 使用路由中间件
 const Routers = require('./app/routers');
